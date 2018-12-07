5f8c0983330138cd0b81c9a3891a46264152bfc3
Neolnevergiveup <1203237924@qq.com>
2018-12-07 23:08:27 +0800

    编辑章节

--- Front_end/build/js/chapter_add_edit.js
+++ Front_end/build/js/chapter_add_edit.js
@@ -1,4 +1,5 @@
 $(function(){
+    // 获得url参数
     function getUrlParam(name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
         var r = window.location.search.substr(1).match(reg);  //匹配目标参数
@@ -9,4 +10,10 @@ $(function(){
     if (para) {
         $('#add_edit_course h3').text('编辑章节');
     }
+    // 根据文件类型选择，切换显示“文字”、“ppt”、“视频”上传框
+    var radio = $('input[name="type"]');
+    radio.change(function(){
+        var index = radio.index(this);
+        $('.toggleShow').eq(index).show().siblings('.toggleShow').hide();
+    })
 })
